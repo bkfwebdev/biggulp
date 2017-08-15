107bb76cd2cbd1e7f87088abe8f2ea20a1cc1b7c
bkfwebdev <bryant.feld@gmail.com>
2017-08-15 17:54:32 -0400

    scripts task is working

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,44 @@
 var myGulp = require("gulp");
 var gulpConcat = require("gulp-concat");
 var gulpMinify = require("gulp-uglify");
+
 //------------------ define task -------------------------------
-myGulp.task("scripts",function(){
-gulp.src(["js/global.js","js/circle/autogrow.js","js/circle/circle.js"])
-.pipe(gulpConcat("appconcat.js"))
-.pipe(gulpMinify("madugly.js"))
-.pipe(gulp.dest("dist/all.min.js"))
+
+// The gulp scripts command concatenates, minifies, and copies all of the project’s JavaScript files into an all.min.js file
+// The command copies the all.min.js file into the dist/scripts folder
+/*myGulp.task("scripts",function(){
+myGulp.src(["js/global.js","js/circle/autogrow.js","js/circle/circle.js"])
+.pipe(gulpConcat("all.mim.js"))
+.pipe(gulpMinify())
+.pipe(myGulp.dest("dist/all.min.js"))
+})
+*/
+myGulp.task("scripts", function() {
+    return myGulp.src(["js/global.js","js/circle/autogrow.js","js/circle/circle.js"])
+        .pipe(gulpConcat("all.min.js"))
+        .pipe(gulpMinify())
+        .pipe(myGulp.dest("dist/"));
 });
 
-myGulp.task("styles",function(){
+// The gulp styles command compiles the project’s SCSS files into CSS, and concatenates and minifies into an all.min.css file
+// The command copies the all.min.css file into the dist/styles folder
+myGulp.task("styles",function(){});
 
-});
-myGulp.task("images",function(){
+// source maps
+// The gulp scripts command generates JavaScript source maps
+// The gulp styles command generates CSS source maps
 
-});
-myGulp.task("clean",function(){
+// The gulp images command copies the optimized images to the dist/content folder.
+myGulp.task("images",function(){});
 
-});
-myGulp.task("build",function(){
+// The gulp clean command deletes all of the files and folders in the dist folder.
+myGulp.task("clean",function(){});
 
-});
+// The gulp build command properly runs the clean, scripts, styles, and images tasks.
+myGulp.task("build",function(){});
+
 //---------------- set up default task ------------------------
-
+// The gulp command properly runs the build task as a dependency
+// The gulp command serves the project using a local webserver.
+// The gulp command also listens for changes to any .scss file. When there is a change to any .scss file, the gulp styles command is run, 
+// the files are compiled, concatenated and minified to the dist folder, and the browser reloads, displaying the changes
