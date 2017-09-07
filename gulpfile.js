aa674bdf42590636acb6b55211b2c6368abddf7e
bkfwebdev <bryant.feld@gmail.com>
2017-09-06 22:12:48 -0400

    clean task works

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var gulpConcat = require("gulp-concat");
 var gulpMinify = require("gulp-uglify");
 var soSassy = require("gulp-sass");
 var mapThat = require("gulp-sourcemaps");
+var cssCrunch = require("gulp-clean-css");
+var concatCss = require('gulp-concat-css'); 
+var imageCrunch = require('gulp-imagemin');
+var cleanUpNice = require('gulp-clean');
 
 
 //------------------ define task -------------------------------
@@ -12,39 +16,50 @@ var mapThat = require("gulp-sourcemaps");
 // The gulp scripts command concatenates, minifies, and copies all of the project’s JavaScript files into an all.min.js file
 // The command copies the all.min.js file into the dist/scripts folder
 
-myGulp.task("scripts", function() {
+myGulp.task("scripts", function(){
     return myGulp.src(["js/global.js","js/circle/autogrow.js","js/circle/circle.js"])
+        .pipe(mapThat.init())
         .pipe(gulpConcat("all.min.js"))
         .pipe(gulpMinify())
-        .pipe(myGulp.dest("dist/"));
+        .pipe(mapThat.write("./"))
+        .pipe(myGulp.dest("dist/scripts"));
 });
 
 // The gulp styles command compiles the project’s SCSS files into CSS, and concatenates and minifies into an all.min.css file
 // The command copies the all.min.css file into the dist/styles folder
-
-myGulp.task("styles",function(){
-    return myGulp.src(["sass/*.sass"]).
-    pipe(soSassy()).
-    pipe(gulpConcat()).
-    pipe(gulpMinify("all.min.css")).
-    pipe(myGulp.dest("dist/"));
-});
-
-// source maps
+// >source maps<
 // The gulp scripts command generates JavaScript source maps
 // The gulp styles command generates CSS source maps
 
+myGulp.task("styles",function(){
+    return myGulp.src(["sass/*.scss"])
+        .pipe(mapThat.init())
+        .pipe(soSassy())
+        .pipe(concatCss("all.min.css"))   
+        .pipe(cssCrunch())
+        .pipe(mapThat.write("./"))
+        .pipe(myGulp.dest("dist/styles")); 
+});
+
 // The gulp images command copies the optimized images to the dist/content folder.
-myGulp.task("images",function(){});
+/*
+myGulp.task("images", function(){
+    return myGulp.src(["images/*.jpg","images/*.png"])
+        .pipe(imageCrunch())
+        .pipe(myGulp.dest("dist/content"))
+});
+*/
 
 // The gulp clean command deletes all of the files and folders in the dist folder.
-myGulp.task("clean",function(){});
+myGulp.task("clean", function () {
+    return myGulp.src("./dist", {read: false})
+        .pipe(cleanUpNice());
+});
 
-// The gulp build command properly runs the clean, scripts, styles, and images tasks.
-myGulp.task("build",function(){});
 
 //---------------- set up default task ------------------------
 // The gulp command properly runs the build task as a dependency
 // The gulp command serves the project using a local webserver.
 // The gulp command also listens for changes to any .scss file. When there is a change to any .scss file, the gulp styles command is run, 
 // the files are compiled, concatenated and minified to the dist folder, and the browser reloads, displaying the changes
+// myGulp.task("default",["build","serveItUp","sassWatch"]);
