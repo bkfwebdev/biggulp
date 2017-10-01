710b4ce5c4fb1809b6a3e8bc08dfa275a8533ae9
bkfwebdev <bryant.feld@gmail.com>
2017-09-30 23:32:18 -0400

    clean up and format gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,10 @@ var concatCss = require("gulp-concat-css");
 var imageCrunch = require("gulp-imagemin");
 var cleanUpNice = require("gulp-clean");
 var serveItUp = require("gulp-webserver");
-
 //------------------ define task -------------------------------
-
 // The gulp scripts command concatenates, minifies, and copies all of the project’s JavaScript files into an all.min.js file
 // The command copies the all.min.js file into the dist/scripts folder
-
+// The gulp scripts command generates JavaScript source maps
 myGulp.task("scripts", function(){
     return myGulp.src(["js/global.js","js/circle/autogrow.js","js/circle/circle.js"])
         .pipe(mapThat.init())
@@ -24,7 +22,9 @@ myGulp.task("scripts", function(){
         .pipe(mapThat.write("./"))
         .pipe(myGulp.dest("dist/scripts"));
 });
-
+// The gulp styles command compiles the project’s SCSS files into CSS, and concatenates and minifies into an all.min.css file
+// The command copies the all.min.css file into the dist/styles folder
+// The gulp styles command generates CSS source maps
 myGulp.task("styles0", function () {
     return myGulp.src("sass/global.scss")
         .pipe(soSassy().on("error", soSassy.logError))
@@ -32,57 +32,25 @@ myGulp.task("styles0", function () {
         .pipe(cssCrunch())
         .pipe(myGulp.dest("dist/styles"));
    });
-
-   myGulp.task("styles1", function () {
+myGulp.task("styles1", function () {
     return myGulp.src("dist/styles/all.min.css")
-     .pipe(mapThat.init())
-     .pipe(mapThat.write("./"))
-     .pipe(myGulp.dest("dist/styles"));
+        .pipe(mapThat.init())
+        .pipe(mapThat.write("./"))
+        .pipe(myGulp.dest("dist/styles"));
    });
-
 myGulp.task("styles",["styles0","styles1"]);
-
-
-
-// The gulp styles command compiles the project’s SCSS files into CSS, and concatenates and minifies into an all.min.css file
-// The command copies the all.min.css file into the dist/styles folder
-// >source maps<
-// The gulp scripts command generates JavaScript source maps
-// The gulp styles command generates CSS source maps
-
-/* myGulp.task("styles1",function(){
-    return myGulp.src(["sass/global.scss"])
-        .pipe(soSassy())
-        .pipe(concatCss("all.min.css"))   
-        .pipe(cssCrunch())
-        .pipe(myGulp.dest("dist/styles")); 
-});
-
-myGulp.task("styles2",function(){
-    return myGulp.src(["dist/styles/all.min.css"])
-    .pipe(mapThat.init())
-    .pipe(mapThat.write("dist/styles"))
-})
-
-myGulp.task("styles",["styles1","styles2"]);
-*/
-
-
 // The gulp images command copies the optimized images to the dist/content folder.
-
 myGulp.task("images", function(){
     return myGulp.src(["images/*.jpg","images/*.png"])
         .pipe(imageCrunch())
         .pipe(myGulp.dest("dist/content"))
 });
- 
-
-// The gulp clean command deletes all of the files and folders in the dist folder.
+ // The gulp clean command deletes all of the files and folders in the dist folder.
 myGulp.task("clean", function () {
     return myGulp.src("./dist", {read: false})
         .pipe(cleanUpNice()); 
 });
-
+// serves the project using a local webserver.
 myGulp.task('webServer', function() {
     return myGulp.src("./")
       .pipe(serveItUp({
@@ -92,12 +60,9 @@ myGulp.task('webServer', function() {
         open: true
       }));
   });
-
-
 myGulp.task("sassWatch",function(){
     return myGulp.watch("sass/*.scss",["build"])
 });
-
 myGulp.task("build",["clean","scripts","styles","images","webServer","sassWatch"]);
 myGulp.task("default",["build"]);
 //---------------- set up default task ------------------------
